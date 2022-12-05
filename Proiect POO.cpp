bebae1e5cd7184ef41689bccef6a14153791f7cb
vpirv <vladparvan12@yahoo.com>
2022-12-05 20:15:38 +0200

    created development branch

--- Proiect POO.cpp
+++ Proiect POO.cpp
@@ -4,14 +4,14 @@
 /*
 Minim 3 clase ce au legătură cu proiectul
 Clasele trebuie să conțină(per total, nu fiecare) 
-	cel puțin un vector alocat dinamic de caractere,	X
-	un vector numeric alocat dinamic,				    X
-	un câmp constant,									X
-	un câmp static										X
-	și o metodă statică.								?
-Membrii trebuie să aibă legătură cu clasa.				X
-Toate atributele vor fi definite în zona privată a clasei		X
-Toate atributele vor avea metode de acces; setterii vor conține validări		X
+	cel puțin un vector alocat dinamic de caractere,									X
+	un vector numeric alocat dinamic,													X
+	un câmp constant,																	X
+	un câmp static																		X
+	și o metodă statică.																?
+Membrii trebuie să aibă legătură cu clasa.												X
+Toate atributele vor fi definite în zona privată a clasei								X
+Toate atributele vor avea metode de acces; setterii vor conține validări				X
 Fiecare clasa va conține cel puțin 2 metode generice(altele decât constructorii sau metodele de acces) ce vor fi utilizate pentru a realiza diverse prelucrări
 Fiecare clasă va conține cel puțin un constructor implicit și unul cu parametri.Clasele cu membri pointeri vor respecta „regula celor 3”.
 Fiecare clasă va conține supraîncărcări pentru operatorii << și >>
